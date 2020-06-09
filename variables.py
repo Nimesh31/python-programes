2999d96f4b72a4a27027f487c73e66a258b45a35
Nimesh31 <54076597+Nimesh31@users.noreply.github.com>
2020-06-09 19:42:11 +0530

    variables.py
    This is how i learned variables in python

---
+++ variables.py
@@ -0,0 +1,14 @@
+character_name = "Nimesh Joshi" #This are strings
+charcter_age = 19 #This is intiger 
+is_male = True #This is boolean
+print("There once was a man name " + character_name + ", ") # Here we are using string variable with print stetement 
+print("he was " + charcter_age + "years old, ") # Here we are using intiger variable with print stetement 
+
+#Now bellow i just repeated the process which we did above
+
+character_name = "roy"
+print("He really liked the name " + character_name + ",")
+print("But did't like being " + charcter_age + ".")
+
+floot = 5.2 #Here this is an floot variable in python
+print(floot) 
